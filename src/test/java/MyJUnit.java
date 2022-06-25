cded920d427da07d9ff58e624f536d9548e9a39d
Farhana Sultana <farhanaSmrity@gmail.com>
2022-06-25 15:13:08 +0600

    initial commit

---
+++ src/test/java/MyJUnit.java
@@ -0,0 +1,32 @@
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+public class MyJUnit {
+    WebDriver driver;
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
+        FirefoxOptions ops=new FirefoxOptions();
+        ops.addArguments("--headed");
+        driver=new FirefoxDriver(ops);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+    }
+
+    @Test
+    public void getTitle() {
+        driver.get("https://demoqa.com/");
+        String title = driver.getTitle();
+        System.out.println(title);
+
+
+    }
+
+}
